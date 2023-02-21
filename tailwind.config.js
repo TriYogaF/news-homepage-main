7dae5e1c3c7319103f3464ddb04810b720f63f96
TriYogaF <triyogaf11@gmail.com>
2023-02-22 02:29:17 +0700

    commit init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    screens:{
+      'desktop':'1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
